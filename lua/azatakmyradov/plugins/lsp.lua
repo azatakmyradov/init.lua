a05b900d313a6243345fe791c3c41b82fcfd33e1
Azat Akmyradov <azat@rebdev.com>
2024-01-30 00:40:02 -0500

    Working on LSP

--- lua/azatakmyradov/plugins/lsp.lua
+++ lua/azatakmyradov/plugins/lsp.lua
@@ -1,37 +1,79 @@
--- Language Server Protocol.
 return {
-    'VonHeikemen/lsp-zero.nvim',
+    "neovim/nvim-lspconfig",
     dependencies = {
-        { 'neovim/nvim-lspconfig' },             -- Required
-        { 'williamboman/mason.nvim' },           -- Optional
-        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        "j-hui/fidget.nvim",
     },
     config = function ()
-        local lsp_zero = require('lsp-zero')
+        require("fidget").setup({})
+        require("mason").setup({})
+        require("mason-lspconfig").setup({
+            ensure_installed = { "rust_analyzer", "lua_ls", "html", "intelephense", "phpactor", "tsserver", "cssls", "eslint", "pest_ls", "prismals", "svelte", "tailwindcss", "volar", "jsonls", },
 
-        lsp_zero.on_attach(function(_, bufnr)
-            lsp_zero.default_keymaps({buffer = bufnr})
-
-            local opts = {buffer = bufnr, remap = false}
-
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
-
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-            ensure_installed = {},
             handlers = {
-                lsp_zero.default_setup,
+                function (server_name)
+                    require("lspconfig")[server_name].setup {}
+                end,
+                ["lua_ls"] = function ()
+                    require("lspconfig").lua_ls.setup {
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim" }
+                                }
+                            }
+                        }
+                    }
+                end,
+            }
+        })
+
+        local cmp = require('cmp')
+        local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
             },
+            window = {
+                -- completion = cmp.config.window.bordered(),
+                -- documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                    { name = 'buffer' },
+                })
+        })
+        vim.diagnostic.config({
+            virtual_text = true,
+            update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = ""
+           }
         })
     end
 }
