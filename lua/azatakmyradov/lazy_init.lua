12b567c4086293a4b293f71a4a5f7ee8901e9367
Azat Akmyradov <azat@rebdev.com>
2024-01-29 22:34:22 -0500

    refactor: remove lazy.nvim change notifications

--- lua/azatakmyradov/lazy_init.lua
+++ lua/azatakmyradov/lazy_init.lua
@@ -12,4 +12,7 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("azatakmyradov.plugins")
+require("lazy").setup({
+    spec = "azatakmyradov.plugins",
+    change_detection = { notify = false }
+})
