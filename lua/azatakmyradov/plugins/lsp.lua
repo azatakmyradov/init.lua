2b7a804adc7c6422114fc49d5f62ea248949534a
Azat Akmyradov <azat@rebdev.com>
2024-01-30 13:16:43 -0500

    feat: Tab and S-Tab will work for luasnip

--- lua/azatakmyradov/plugins/lsp.lua
+++ lua/azatakmyradov/plugins/lsp.lua
@@ -49,7 +49,7 @@ return {
         })
 
         local cmp = require('cmp')
-        local cmp_select = {behavior = cmp.SelectBehavior.Select}
+        local luasnip = require('luasnip')
 
         cmp.setup({
             snippet = {
@@ -57,15 +57,27 @@ return {
                     require('luasnip').lsp_expand(args.body)
                 end,
             },
-            window = {
-                -- completion = cmp.config.window.bordered(),
-                -- documentation = cmp.config.window.bordered(),
-            },
             mapping = cmp.mapping.preset.insert({
-                ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<Enter>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
