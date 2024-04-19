c2b5cb5dcd570eea6498e6d792d9906cd51fe7b5
Azat Akmyradov <azat@rebdev.com>
2024-04-19 16:44:08 -0400

    feat: add status line

--- lua/azatakmyradov/plugins/init.lua
+++ lua/azatakmyradov/plugins/init.lua
@@ -106,5 +106,14 @@ return {
             'nvim-lua/plenary.nvim',
         },
         config = true,
+     },
+
+    -- Status Line
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup()
+        end
     }
 }
