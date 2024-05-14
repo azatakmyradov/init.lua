40cb8c92b4def0b49f8f6043334cb9291cbb4efa
Azat Akmyradov <azat@rebdev.com>
2024-05-14 14:46:13 -0400

    Add go to neotest

--- lua/azatakmyradov/plugins/neotest.lua
+++ lua/azatakmyradov/plugins/neotest.lua
@@ -6,11 +6,24 @@ return {
         "nvim-treesitter/nvim-treesitter",
         'V13Axel/neotest-pest',
         "nvim-neotest/nvim-nio",
+        "nvim-neotest/neotest-go",
     },
     config = function()
+        local neotest_ns = vim.api.nvim_create_namespace("neotest")
+        vim.diagnostic.config({
+            virtual_text = {
+                format = function(diagnostic)
+                    local message =
+                        diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+                    return message
+                end,
+            },
+        }, neotest_ns)
+
         require('neotest').setup({
             adapters = {
                 require('neotest-pest'),
+                require("neotest-go"),
             }
         })
 
