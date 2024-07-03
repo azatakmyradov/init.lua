c8bf208114f70d93f60d6c9b75bf21b9ca4c4951
Azat Akmyradov <azat@rebdev.com>
2024-07-03 16:20:29 -0400

    add codesnap

--- lua/azatakmyradov/plugins/init.lua
+++ lua/azatakmyradov/plugins/init.lua
@@ -112,5 +112,19 @@ return {
         config = function()
             require('lualine').setup()
         end
-    }
+    },
+
+    {
+        "mistricky/codesnap.nvim",
+        build = "make",
+        keys = {
+            { "<leader>cc", "<cmd>CodeSnap<cr>",     mode = "x", desc = "Save selected code snapshot into clipboard" },
+            { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+        },
+        opts = {
+            save_path = "~/Pictures",
+            has_line_number = true,
+            title = "Akmyradov",
+        }
+    },
 }
